a2233873b54bbbe73d50694c1b3325ca789a4484
Muneeb <muneebchaudhry.dev@gmail.com>
2023-12-19 21:51:47 +0500

    Change Styles

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -36,8 +36,8 @@ export default async function Login() {
             <h3>Welcome</h3>
             <Image src="/assets/icons/hand.png" alt="hand" width={30} height={30} />
             <p>Please login here</p>
-            <LoginForm />
           </div>
+          <LoginForm />
         </div>
       </div>
     </section>
